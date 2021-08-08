a6aeb528b0506bd4e5b02d1faf52010931558884
stzups <griffinht@gmail.com>
2021-08-08 18:37:05 -0400

    add biome type

--- converter/src/main/java/net/stzups/civref/converter/RealisticBiomes.java
+++ converter/src/main/java/net/stzups/civref/converter/RealisticBiomes.java
@@ -2,13 +2,14 @@ package net.stzups.civref.converter;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import net.stzups.civref.converter.minecraft.Biome;
 
 import java.util.Map;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RealisticBiomes {
     @JsonProperty("biome_aliases")
-    private Map<String, String[]> biomeAliases;
+    private Map<String, Biome[]> biomeAliases;
 
     @Override
     public String toString() {
