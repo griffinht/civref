59f18c58ebd593beb6ba8e789864785663b7fce9
stzups <griffinht@gmail.com>
2021-08-08 20:28:35 -0400

    fix unsafe casts

--- converter/src/main/java/net/stzups/civref/converter/RealisticBiomes.java
+++ converter/src/main/java/net/stzups/civref/converter/RealisticBiomes.java
@@ -1,16 +1,25 @@
 package net.stzups.civref.converter;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class RealisticBiomes {
-    private Map<String, String[]> biomeAliases;
+    private Map<String, String[]> biomeAliases = new HashMap<>();
     private Map<String, Plant> plants = new HashMap<>();
 
-    public RealisticBiomes(Map<String, ?>  realisticBiomes) {
-        this.biomeAliases = (Map<String, String[]>) realisticBiomes.get("biome_aliases");
-        for (Map.Entry<String, Map<String, ?>> entry : ((Map<String, Map<String, ?>>)realisticBiomes.get("plants")).entrySet()) {
-            plants.put(entry.getKey(), new Plant(entry.getValue()));
+    public RealisticBiomes(Object realisticBiomes) {
+        Map<?, ?> map = ((Map<?, ?>) realisticBiomes);
+        for (Map.Entry<?, ?> entry : ((Map<?, ?>) map.get("biome_aliases")).entrySet()) {
+            Object[] objects = ((List<?>) entry.getValue()).toArray();
+            String[] strings = new String[objects.length];
+            for (int i = 0; i < objects.length; i++) {
+                strings[i] = objects[i].toString();
+            }
+            biomeAliases.put(entry.getKey().toString(), strings);
+        }
+        for (Map.Entry<?, ?> entry : ((Map<?, ?>) map.get("plants")).entrySet()) {
+            plants.put(entry.getKey().toString(), new Plant(entry.getValue()));
         }
     }
 }
@@ -18,14 +27,8 @@ public class RealisticBiomes {
 class Plant {
     private String persistentGrowthPeriod;
 
-    public Plant(Map<String, ?> plant) {
-        Object object = plant.get("persistent_growth_period");
-        if (object instanceof String) {
-            persistentGrowthPeriod = (String) object;
-        } else if (object instanceof Integer) {
-            persistentGrowthPeriod = ((Integer) object).toString();
-        } else {
-            throw new ClassCastException(object + " is not a " + String.class + " or " + Integer.class);
-        }
+    public Plant(Object plant) {
+        Map<?, ?> map = (Map<?, ?>) plant;
+        persistentGrowthPeriod = map.get("persistent_growth_period").toString();
     }
 }
