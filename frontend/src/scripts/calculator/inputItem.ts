3a8642be506f8e7d7eef4ab04a349a760f728bdf
stzups <griffinht@gmail.com>
2021-08-17 12:19:41 -0400

    add label to amount input

--- frontend/src/scripts/calculator/inputItem.ts
+++ frontend/src/scripts/calculator/inputItem.ts
@@ -8,11 +8,21 @@ export default function createInputItem(plant: Plant, time: Data<number>) {
     element.style.display = 'flex'
     element.style.border = '1px solid black'
     {
-        let e = document.createElement('input')
-        e.setAttribute('type', 'number')
-        e.addEventListener('input', () => {
-            amount.update(parseInt(e.value))
-        })
+        let e = document.createElement('div')
+        e.style.display = 'flex'
+        {
+            let ee = document.createElement('h3')
+            ee.innerText = 'Amount: '
+            e.append(ee)
+        }
+        {
+            let ee = document.createElement('input')
+            ee.setAttribute('type', 'number')
+            ee.addEventListener('input', () => {
+                amount.update(parseInt(ee.value))
+            })
+            e.append(ee)
+        }
         element.append(e)
     }
     element.append(plant.getElement())
