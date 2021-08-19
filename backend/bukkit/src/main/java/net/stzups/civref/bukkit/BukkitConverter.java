70bb921e3adc16a3c7f60643b8b1b3a8389a3c25
stzups <griffinht@gmail.com>
2021-08-18 21:10:08 -0400

    tiny change to BukkitConverter

--- backend/bukkit/src/main/java/net/stzups/civref/bukkit/BukkitConverter.java
+++ backend/bukkit/src/main/java/net/stzups/civref/bukkit/BukkitConverter.java
@@ -1,27 +1,45 @@
 package net.stzups.civref.bukkit;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.WorldCreator;
 import org.bukkit.block.Block;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BukkitConverter extends JavaPlugin {
+    private String worldName = "world";
+    private Material[] materials = new Material[] {
+            Material.WHEAT,
+            Material.POTATO,
+            Material.PODZOL,
+            Material.ACACIA_BOAT,
+        };
+    private int iterations = 100;
+
     @Override
     public void onEnable() {
-        World world = org.bukkit.Bukkit.getWorld("world");
+        World world = Bukkit.getWorld(worldName);
         if (world == null) {
-            getLogger().warning("World is null");
-            return;
+            world = Bukkit.createWorld(new WorldCreator(worldName)); //todo check if this actually works
+            if (world == null) {
+                throw new RuntimeException("Failed to find or create world with name " + worldName);
+            }
         }
 
-        getLogger().info(world.toString());
-        Block block = world.getBlockAt(255, 69, 78);
+        getLogger().info("Using " + world.getName());
+        Block block = world.getBlockAt(0, 0, 0);
 
-        for (int i = 0; i < 100; i++) {
-            for (ItemStack itemStack : block.getDrops()) {
-                getLogger().info(itemStack.toString());
+        for (Material material : materials) {
+            block.setType(material);
+
+            for (int i = 0; i < iterations; i++) {
+                for (ItemStack itemStack : block.getDrops()) {
+                    getLogger().info(itemStack.toString());
+                }
+                getLogger().info("done");
             }
-            getLogger().info("done");
         }
     }
 }
